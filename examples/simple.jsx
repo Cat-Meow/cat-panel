433c2b82a5b5e42475c18ae00ebddc82df2127be
yanweiqing <ywq1991@gmail.com>
2016-01-23 15:23:33 +0800

    panle init

---
+++ examples/simple.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Panel from '../src/panel.js';
+import '../assets/index.less';
+
+export default class Example extends Component {
+    render () {
+        return (
+            <Panel
+                header='展示'
+                footer='测试'
+                myStyle='info'
+            >
+                <Panel.Body>
+                    <p>Panel</p>
+                </Panel.Body>
+                <Panel.Ul>
+                    <div>1</div>
+                    <div>2</div>
+                </Panel.Ul>
+            </Panel>
+        )
+    }
+}
